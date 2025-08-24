fb642b0c8bc5a359d4c7f8991898a441efb5f985
Manuel Duarte  <carlosd99m@gmail.com>
2025-08-24 03:46:35 -0600

    wip: initial code

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_graphql_albums/app/app.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-
-void main() async {
-  await initHiveForFlutter();
-
-  runApp(const MyApp());
-}
